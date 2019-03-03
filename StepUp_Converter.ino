8efbc153b7dba0e8fb7c0c4ea804da77dee02a64
diveapnea <41834461+diveapnea@users.noreply.github.com>
2019-03-03 21:29:12 +0100

    protections added

--- StepUp_Converter.ino
+++ StepUp_Converter.ino
@@ -24,11 +24,13 @@ uint8_t pwm = 0;
 uint8_t pwm_max = 0;
 
 void setup() {
+  //Serial.begin(9600); //max 10000 or maybe even 20000
   // put your setup code here, to run once:
-  
-  //analogWrite(D5, pwm); //PWM on D5  analogWrite values from 0 to 255  
-  //TCCR0B = TCCR0B & 0b11111000 | 0x01; //Pin5 0x01 --> 62,5kHz pwm
-
+ /*
+  analogWrite(D5, pwm); //PWM on D5  analogWrite values from 0 to 255  
+  TCCR0B = TCCR0B & 0b11111000 | 0x03; //Pin5 0x01 --> 62,5kHz pwm
+  pwm_max = 255 * 0.9;
+*/
   /*
   pinMode(D3, OUTPUT); // output pin for OCR2B
   // Set up the 250KHz output @ D3
@@ -42,22 +44,40 @@ void setup() {
   // Set up the fast PWM up to 8Mhz output @D5
   TCCR0A = _BV(COM0A1) | _BV(COM0B1) | _BV(WGM01) | _BV(WGM00);
   TCCR0B = _BV(WGM02) | _BV(CS00);
-  OCR0A = 15; //freq. divider // 1--> 8MHz,1bit pwm, 3-->4Mhz,2bit pwm, 7-->2Mhz,3bit pwm, 15-->1Mhz,4bit pwm etc.
-  OCR0B = 0; //duty cycle (0 means 50% for 1 bit)
+  OCR0A = 255; //freq. divider // 1--> 8MHz,1bit pwm, 3-->4Mhz,2bit pwm, 7-->2Mhz,3bit pwm, 15-->1Mhz,4bit pwm etc.
+  OCR0B = OCR0A/2; //duty cycle (0 means 50% for 1 bit)
 
-  pwm_max= OCR0A * 0.9; //if standard 62,5 kHz osc. used, OCR0A is zero. So replace with 255
+  pwm_max= OCR0A * 0.8; //if standard 62,5 kHz osc. used, OCR0A is zero. So replace with 255
+
+
 }
 
 
 void loop() {
 
-
-  float voltage = analogRead(A6) * (30.0*0.9961/ 1023.0); //voltage divider used to sense 5V at 30V real//0.9961 is the calibration value
-
-  if (voltage < 12.5 & pwm < pwm_max) pwm = pwm+1; //do not go to 100% PWM, which does not move anything. 90% seems to be a golden value
-  else if (pwm > 0) pwm = pwm-1; //underflow can occur
+  int raw_analog_reading = analogRead(A6); //remember ADC is 10bit
   
-  analogWrite(D5, pwm); //PWM on D5  analogWrite values from 0 to 255  
-  delay(50); // delay x ms
+  //I/O over voltage protection
+  if(raw_analog_reading > 1020){ //approaching saturation
+    pwm = 0;
+    analogWrite(D5, pwm);
+  }
+  
+  float voltage = raw_analog_reading * (30.0*0.9961/ 1023.0); //voltage divider used to sense 5V at 30V real//0.9961 is the calibration value
+
+  if (voltage < 13.0 & pwm < pwm_max) pwm = pwm+1; //do not go to 100% PWM, which does not move anything. 80-90% seems to be a golden value
+  else if (pwm > 0) pwm = pwm-1; //underflow can occur
+
+  // fly away protection. Seems not to make sense but it works. Checks whether a small PWM change leverages the voltage or not
+  if(pwm > raw_analog_reading) pwm = 0;
+  
+  
+  //this command is same as assigning a value to OCR0B
+  analogWrite(D5, pwm); //PWM on D5  analogWrite values from 0 to 255 or OCR0A if non-zero
+  
+  delay(62500/2000); // delay x ms --> 1 s becomes 62500 due to change of the clock multiplier
+  
+  //String result = raw_analog_reading + String(" ") + pwm;
+  //Serial.println(result);
 
 }
