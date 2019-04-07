923880f5e66b5ed896d673cf68d9f234a8dfbcf7
diveapnea <41834461+diveapnea@users.noreply.github.com>
2019-04-07 20:12:53 +0200

    Current sensor curve corrected

--- AmpMeter_Fast.ino
+++ AmpMeter_Fast.ino
@@ -1,23 +1,23 @@
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(1000000); //max 1000000
-  pinMode(D3, OUTPUT);
-
+  Serial.begin(2000000); //max 1000000
 }
 
 void loop() {
 
   // put your main code here, to run repeatedly:
   
-  //current sensor ACS712T
-  //-30A --> 0,5V
-  //0A --> 2,5V
-  //30A --> 4,5V
+ // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):  
+  //float voltage = A7_val * (5.0 / 1023.0);
+  //convert voltage into current (0.5V-->-30A, 2,5V-->0A, 4.5V-->+30A: f_current(x)=15x-37.5
+  //f_current(A7_val) =  (15 * A7_val * 5.0 / 1023.0) - 37.5 --> 0.073313 * A7_val - 37.5
   
-  //combining voltage and current conversions AND approximation gives out: f_current(x) = 0.0586510264*A7_val-30
-  
-  float current = 0.05865*analogRead(A7)-30;
+  //convert voltage into current (1.5V-->-5A, 2,5V-->0A, 3.5V-->+5A: f_current(x)=5x-12.5  
+  //f_current(A7_val) =  (5 * A7_val * 5.0 / 1023.0) - 12.5 --> 0.024438 * A7_val - 12.5
+
+      
+  float current = 0.024438*analogRead(A7)-12.5;
   float voltage = analogRead(A6) * (5.0 / 1023.0);
 
   Serial.println(current,1); //low precision for high speed. 0.1A is the best we can get here
